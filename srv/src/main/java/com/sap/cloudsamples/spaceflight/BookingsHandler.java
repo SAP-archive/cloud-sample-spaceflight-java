a4775974dc370d461847578e80d4af11b8c58075
Christian Georgi <christian.georgi@sap.com>
2018-07-17 14:55:52 +0200

    Validation for booking.itineraries

--- srv/src/main/java/com/sap/cloudsamples/spaceflight/BookingsHandler.java
+++ srv/src/main/java/com/sap/cloudsamples/spaceflight/BookingsHandler.java
@@ -1,5 +1,7 @@
 package com.sap.cloudsamples.spaceflight;
 
+import java.util.Collections;
+
 import org.apache.http.HttpStatus;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -7,11 +9,13 @@ import org.slf4j.LoggerFactory;
 import com.sap.cloud.sdk.odatav2.connectivity.ODataException;
 import com.sap.cloud.sdk.s4hana.connectivity.ErpConfigContext;
 import com.sap.cloud.sdk.s4hana.datamodel.odata.namespaces.businesspartner.BusinessPartner;
+import com.sap.cloud.sdk.service.prov.api.DataSourceHandler;
 import com.sap.cloud.sdk.service.prov.api.EntityData;
 import com.sap.cloud.sdk.service.prov.api.EntityDataBuilder;
 import com.sap.cloud.sdk.service.prov.api.ExtensionHelper;
 import com.sap.cloud.sdk.service.prov.api.annotations.BeforeCreate;
 import com.sap.cloud.sdk.service.prov.api.annotations.BeforeUpdate;
+import com.sap.cloud.sdk.service.prov.api.exception.DatasourceException;
 import com.sap.cloud.sdk.service.prov.api.exits.BeforeCreateResponse;
 import com.sap.cloud.sdk.service.prov.api.exits.BeforeUpdateResponse;
 import com.sap.cloud.sdk.service.prov.api.exits.PreExtensionResponseBuilderWithBody;
@@ -24,39 +28,42 @@ import com.sap.cloudsamples.spaceflight.s4.BusinessPartnerRead;
 
 public class BookingsHandler {
 
+	private static final String PROPERTY_ID = "ID";
 	private static final String BOOKING_SERVICE = "BookingService";
+	private static final String ENTITY_ITINERARIES = BOOKING_SERVICE + ".Itineraries";
 	private static final String ENTITY_BOOKINGS = "Bookings";
-	private static final String PROPERTY_CUSTOMER = "Customer";
-	private static final String PROPERTY_EMAIL = "EmailAddress";
+	private static final String PROPERTY_BOOKING_ITINERARYID = "Itinerary_ID";
+	private static final String PROPERTY_BOOKING_CUSTOMER = "Customer";
+	private static final String PROPERTY_BOOKING_EMAIL = "EmailAddress";
 
 	private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
 	@BeforeCreate(serviceName = BOOKING_SERVICE, entity = ENTITY_BOOKINGS)
 	public BeforeCreateResponse beforeBookingCreate(CreateRequest req, ExtensionHelper helper)
-			throws ODataException {
-		return beforeUpsert(req.getData());
+			throws ODataException, DatasourceException {
+		return beforeUpsert(req.getData(), helper.getHandler());
 	}
 
 	@BeforeUpdate(serviceName = BOOKING_SERVICE, entity = ENTITY_BOOKINGS)
-	public BeforeUpdateResponse beforeBookingUpdate(UpdateRequest req, ExtensionHelper helper) throws ODataException {
-		return beforeUpsert(req.getData());
+	public BeforeUpdateResponse beforeBookingUpdate(UpdateRequest req, ExtensionHelper helper)
+			throws ODataException, DatasourceException {
+		return beforeUpsert(req.getData(), helper.getHandler());
 	}
 
-	private PreExtensionResponseWithBody beforeUpsert(EntityData reqData) throws ODataException {
+	private PreExtensionResponseWithBody beforeUpsert(EntityData reqData, DataSourceHandler dataSource)
+			throws ODataException, DatasourceException {
 		EntityDataBuilder entityBuilder = EntityData.getBuilder(reqData);
 		BusinessPartner bPartner = null;
 
 		// fetch BP
-		if (reqData.contains(PROPERTY_CUSTOMER)) {
-			String customerId = (String) reqData.getElementValue(PROPERTY_CUSTOMER);
+		if (reqData.contains(PROPERTY_BOOKING_CUSTOMER)) {
+			String customerId = (String) reqData.getElementValue(PROPERTY_BOOKING_CUSTOMER);
 
 			try {
 				bPartner = new BusinessPartnerRead(new ErpConfigContext(), customerId).execute();
 			} catch (Exception e) {
 				logger.info(e.getMessage(), e);
-				return new PreExtensionResponseImpl(ErrorResponse.getBuilder().setStatusCode(HttpStatus.SC_BAD_REQUEST)
-						.setMessage("NoSuchCustomer", customerId)
-						.addErrorDetail("NoSuchCustomer", PROPERTY_CUSTOMER, customerId).response());
+				return constraintError("NoSuchCustomer", customerId, PROPERTY_BOOKING_CUSTOMER);
 			}
 			CustomersHandler.completeResponseData(bPartner, true);
 
@@ -64,11 +71,27 @@ public class BookingsHandler {
 		}
 
 		// fetch email from BP
-		if (bPartner != null && !reqData.contains(PROPERTY_EMAIL)) {
-			entityBuilder.addElement(PROPERTY_EMAIL, bPartner.getCustomField(CustomersHandler.PROPERTY_CUSTOMERS_EMAIL))
+		if (bPartner != null && !reqData.contains(PROPERTY_BOOKING_EMAIL)) {
+			entityBuilder
+					.addElement(PROPERTY_BOOKING_EMAIL,
+							bPartner.getCustomField(CustomersHandler.PROPERTY_CUSTOMERS_EMAIL))
 					.buildEntityData(ENTITY_BOOKINGS);
 		}
 
+		if (reqData.contains(PROPERTY_BOOKING_ITINERARYID)) {
+			Object itineraryId = reqData.getElementValue(PROPERTY_BOOKING_ITINERARYID);
+			EntityData itinerary = dataSource.executeRead(ENTITY_ITINERARIES,
+					Collections.singletonMap(PROPERTY_ID, itineraryId), Collections.singletonList(PROPERTY_ID));
+			if (itinerary == null) {
+				return constraintError("NoSuchItinerary", itineraryId, PROPERTY_BOOKING_ITINERARYID);
+			}
+		}
+
 		return new PreExtensionResponseBuilderWithBody(entityBuilder.buildEntityData(ENTITY_BOOKINGS)).response();
+	}
+
+	private static PreExtensionResponseWithBody constraintError(String messageKey, Object id, String target) {
+		return new PreExtensionResponseImpl(ErrorResponse.getBuilder().setStatusCode(HttpStatus.SC_BAD_REQUEST)
+				.setMessage(messageKey, id).addErrorDetail(messageKey, target, id).response());
 	}
 }
