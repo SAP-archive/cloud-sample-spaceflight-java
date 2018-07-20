7cc3697dcc9f34e049b12cd9e574efacd0bdaf0d
Christian Georgi <christian.georgi@sap.com>
2018-07-20 14:21:00 +0200

    Fix entity/perty names, collect errors

--- srv/src/main/java/com/sap/cloudsamples/spaceflight/BookingsHandler.java
+++ srv/src/main/java/com/sap/cloudsamples/spaceflight/BookingsHandler.java
@@ -24,17 +24,16 @@ import com.sap.cloud.sdk.service.prov.api.exits.PreExtensionResponseWithBody;
 import com.sap.cloud.sdk.service.prov.api.request.CreateRequest;
 import com.sap.cloud.sdk.service.prov.api.request.UpdateRequest;
 import com.sap.cloud.sdk.service.prov.api.response.ErrorResponse;
+import com.sap.cloud.sdk.service.prov.api.response.ErrorResponseBuilder;
 import com.sap.cloudsamples.spaceflight.s4.BusinessPartnerRead;
 
 public class BookingsHandler {
 
 	private static final String PROPERTY_ID = "ID";
 	private static final String BOOKING_SERVICE = "BookingService";
-	private static final String ENTITY_EARTHITINERARIES = BOOKING_SERVICE + ".EarthItineraries";
-	private static final String ENTITY_SPACEITINERARIES = BOOKING_SERVICE + ".SpaceItineraries";
+	private static final String ENTITY_ITINERARIES = BOOKING_SERVICE + ".Itineraries";
 	private static final String ENTITY_BOOKINGS = "Bookings";
-	private static final String PROPERTY_BOOKING_EARTHITINERARYID = "EarthItinerary_ID";
-	private static final String PROPERTY_BOOKING_SPACEITINERARYID = "SpaceItinerary_ID";
+	private static final String PROPERTY_BOOKING_ITINERARYID = "Itinerary_ID";
 	private static final String PROPERTY_BOOKING_CUSTOMER = "Customer";
 	private static final String PROPERTY_BOOKING_EMAIL = "EmailAddress";
 
@@ -54,18 +53,12 @@ public class BookingsHandler {
 
 	private PreExtensionResponseWithBody beforeUpsert(EntityData reqData, DataSourceHandler dataSource)
 			throws ODataException, DatasourceException {
+		ErrorResponseBuilder errorResponseBuilder = ErrorResponse.getBuilder();
 		EntityDataBuilder entityBuilder = EntityData.getBuilder(reqData);
 		BusinessPartner bPartner = null;
 
-		PreExtensionResponseWithBody response = validateItinerary(reqData, ENTITY_EARTHITINERARIES,
-				PROPERTY_BOOKING_EARTHITINERARYID, dataSource);
-		if (response != null) {
-			return response;
-		}
-		response = validateItinerary(reqData, ENTITY_SPACEITINERARIES, PROPERTY_BOOKING_SPACEITINERARYID, dataSource);
-		if (response != null) {
-			return response;
-		}
+		errorResponseBuilder = validateItinerary(reqData, ENTITY_ITINERARIES, PROPERTY_BOOKING_ITINERARYID,
+				dataSource, errorResponseBuilder);
 
 		// fetch BP
 		if (reqData.contains(PROPERTY_BOOKING_CUSTOMER)) {
@@ -73,13 +66,13 @@ public class BookingsHandler {
 
 			try {
 				bPartner = new BusinessPartnerRead(new ErpConfigContext(), customerId).execute();
+				CustomersHandler.completeResponseData(bPartner, true);
+				entityBuilder.addElement("CustomerName", bPartner.getBusinessPartnerFullName());
 			} catch (Exception e) {
 				logger.info(e.getMessage(), e);
-				return constraintError("NoSuchCustomer", customerId, PROPERTY_BOOKING_CUSTOMER);
+				errorResponseBuilder = constraintError("NoSuchCustomer", customerId, PROPERTY_BOOKING_CUSTOMER,
+						errorResponseBuilder);
 			}
-			CustomersHandler.completeResponseData(bPartner, true);
-
-			entityBuilder.addElement("CustomerName", bPartner.getBusinessPartnerFullName());
 		}
 
 		// fetch email from BP
@@ -90,24 +83,32 @@ public class BookingsHandler {
 					.buildEntityData(ENTITY_BOOKINGS);
 		}
 
+		if (errorResponseBuilder != null) {
+			return new PreExtensionResponseImpl(errorResponseBuilder.response());
+		}
+
 		return new PreExtensionResponseBuilderWithBody(entityBuilder.buildEntityData(ENTITY_BOOKINGS)).response();
 	}
 
-	private static PreExtensionResponseWithBody validateItinerary(EntityData reqData, String entity, String property,
-			DataSourceHandler dataSource) throws DatasourceException {
+	private static ErrorResponseBuilder validateItinerary(EntityData reqData, String entity, String property,
+			DataSourceHandler dataSource, ErrorResponseBuilder responseBuilder) throws DatasourceException {
 		if (reqData.contains(property)) {
 			Object itineraryId = reqData.getElementValue(property);
 			EntityData itinerary = dataSource.executeRead(entity, Collections.singletonMap(PROPERTY_ID, itineraryId),
 					Collections.singletonList(PROPERTY_ID));
 			if (itinerary == null) {
-				return constraintError("NoSuchItinerary", itineraryId, property);
+				return constraintError("NoSuchItinerary", itineraryId, property, responseBuilder);
 			}
 		}
-		return null;
+		return responseBuilder;
 	}
 
-	private static PreExtensionResponseWithBody constraintError(String messageKey, Object id, String target) {
-		return new PreExtensionResponseImpl(ErrorResponse.getBuilder().setStatusCode(HttpStatus.SC_BAD_REQUEST)
-				.setMessage(messageKey, id).addErrorDetail(messageKey, target, id).response());
+	private static ErrorResponseBuilder constraintError(String messageKey, Object id, String target,
+			ErrorResponseBuilder responseBuilder) {
+		if (responseBuilder == null) {
+			responseBuilder = ErrorResponse.getBuilder();
+		}
+		return responseBuilder.setStatusCode(HttpStatus.SC_BAD_REQUEST).setMessage(messageKey, id)
+				.addErrorDetail(messageKey, target, id);
 	}
 }
