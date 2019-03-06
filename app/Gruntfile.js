14c72681894426c017d1ace2ae1024ff0f6bb125
avizov <alex.avizov@sap.com>
2019-03-06 09:54:45 +0200

    Update Gruntfile.js

--- app/Gruntfile.js
+++ app/Gruntfile.js
@@ -1,6 +1,6 @@
 module.exports = function (grunt) {
 	"use strict";
-	grunt.loadNpmTasks("@sap/grunt-sapui5module-bestpractice-build");
+	grunt.loadNpmTasks("@sap/grunt-sapui5-bestpractice-build");
 	grunt.registerTask("default", [
 		"lint"
 	]);
